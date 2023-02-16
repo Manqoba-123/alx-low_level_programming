1afad90f56e25a30b88f038271db863131ec8622
Manqoba-123 <manoba231@gmail.com>
2023-02-16 13:31:34 -0800

    ngaze fixx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	long long int i;
-	long int d;
+	char a;
 	int b;
-	char c;
-	float a;
+	long int c;
+	long long int d;
+	float e;
 
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 	return (0);
 }
