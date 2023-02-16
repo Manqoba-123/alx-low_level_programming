b275d8c301f9182844df2b35009ee17a5ea49add
Manqoba-123 <manoba231@gmail.com>
2023-02-16 12:05:30 -0800

    Hot Fixgit add .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - The programme that outputs with a help of puts function
+ * Description: output Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
