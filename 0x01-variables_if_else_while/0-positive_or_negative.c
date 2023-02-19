1a63a8b3cc7666286d08dbe58d3f1cea60d6313d
Manqoba-123 <manoba231@gmail.com>
2023-02-19 07:13:14 -0800

    The hot fix of negs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,22 +9,23 @@
  */
 int main(void)
 {
-	int randomNumber;
+	int n;
 
 	srand(time(0));
-	randomNumber = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
-	if (randomNumber > 0)
+	printf("%d", n);
+	if (n > 0)
 	{
-		printf("%d is positive\n", randomNumber);
+		printf("%d is positive\n");
 	}
-	else if (randomNumber == 0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n", randomNumber);
+		printf("%d is zero\n");
 	}
 	else
 	{
-		printf("%d is negative\n", randomNumber);
+		printf("%d is negative\n");
 	}
 	return (0);
 }
