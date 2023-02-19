1ffb3ca7f73838a047e0bf8a5f88908c4ba29c9d
Manqoba-123 <manoba231@gmail.com>
2023-02-19 11:02:33 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: outputs lower case alphabets
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	return (0);
+}
