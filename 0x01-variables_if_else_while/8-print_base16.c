dcdb241c27c82a84a1990ddc05deb59f4d6e9795
Manqoba-123 <manoba231@gmail.com>
2023-02-20 02:11:08 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: hexadecimal base 16
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int numm = 0;
+	char alphas = 'a';
+
+	while (num < 10)
+	{
+		putchar((char)(numm + '0'));
+		numm++;
+	}
+	while (alphas <= 'f')
+	{
+		putchar(alphas);
+		alphas++;
+	}
+	putchar("\n");
+	return (0);
+}
