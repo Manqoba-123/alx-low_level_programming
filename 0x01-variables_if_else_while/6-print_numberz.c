1e0ce802682db7608dbe9ec16c6938b825332893
Manqoba-123 <manoba231@gmail.com>
2023-02-20 01:45:46 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: out puts numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar((char)(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
