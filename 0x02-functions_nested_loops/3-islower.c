7543366798deeac902554aeb0c0d2c34d70b2aa3
Manqoba-123 <manoba231@gmail.com>
2023-02-21 12:15:49 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> checks the cases
+ * @c: char argument
+ * Return: returns 1 0r 0 depending on given condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
