8d731b99756211bc737cf281ce6eaff022ebbfc4
Manqoba-123 <manoba231@gmail.com>
2023-02-21 14:23:54 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> outputs the last digit
+ * @n: char
+ * Return: returns 0
+ */
+int print_last_digit(int n)
+{
+	int last_dig;
+
+	last_dig = n % 10;
+	return (last_dig);
+}
