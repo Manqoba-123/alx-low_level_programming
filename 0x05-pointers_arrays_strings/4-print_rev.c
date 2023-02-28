1fcf2217313af173d618dd0561b492152d115ecb
Manqoba-123 <manoba231@gmail.com>
2023-02-28 08:50:49 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev -> reverse the string
+ * @s: char s
+ * Return: returns void
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+	i = len - 1;
+
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
