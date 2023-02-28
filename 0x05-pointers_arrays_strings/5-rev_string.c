b40dc7c36a79431a1b828606e83b7a209bfeef2c
Manqoba-123 <manoba231@gmail.com>
2023-02-28 10:21:58 -0800

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string -> prints out the reverse of a string
+ * @s: character string
+ * Return: returns void
+ */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char *a, *b, temp;
+
+	len = strlen(s);
+	a = s;
+	b = s;
+
+	for (i = 0; i < len - 1; i++)
+		b++;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = b;
+		*b = *a;
+		*a = temp;
+		a++;
+		b--;
+	}
+	_putchar('\n');
+}
