17f66db8d6440114f16a54f1796c85327b782b18
Manqoba-123 <manoba231@gmail.com>
2023-02-28 03:29:05 -0800

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen -> output the length of a string
+ * @s: it's a striing char
+ * Return: returns an integer
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = strlen(s);
+}
