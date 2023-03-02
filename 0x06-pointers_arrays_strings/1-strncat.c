9962b4778bc05d20bdac32623371ea6cd88404fe
Manqoba-123 <manoba231@gmail.com>
2023-03-02 02:33:33 -0800

    nth conc

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,6 +11,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strcat(dest, src, n);
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+		ptr++;
+	while (*src != '\0' && n-- > 0)
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
 	return (*dest);
 }
