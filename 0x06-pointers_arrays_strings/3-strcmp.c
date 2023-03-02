a52480a4a91116fa7c756d4b40c7c90eb788dc4b
Manqoba-123 <manoba231@gmail.com>
2023-03-02 03:24:07 -0800

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int j;
+	int i;
 
-	j = 0;
-	while (s1[j] != '\0' && s2[j] != '\0')
+	i = 0;
+	while (s1[i] == s2[i])
 	{
-		if (s1[j] != s2[j])
+		if (s1[i] == '\0')
 		{
-			return (s1[j] - s2[j]);
+			return (0);
 		}
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
