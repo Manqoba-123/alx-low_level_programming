40c658fa38e24d32eaab579d62798ee5c8f4f080
Manqoba-123 <manoba231@gmail.com>
2023-03-07 03:19:13 -0800

    1-mem

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy -> copies memory
+ * @dest: destinition
+ * @src: source
+ * @n: no. of bytes
+ * Return: memory copied
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i, j;
+
+	j = n;
+	for (i = 0; i < j; i++)
+	{
+		dest[i] src[i];
+		n--;
+	}
+	return (dest);
+}
