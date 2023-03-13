ec624934a4c5cb339ec33018a0ab6a04ff97c5da
Manqoba-123 <manoba231@gmail.com>
2023-03-13 06:14:19 -0700

    argg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -> prints arguments
+ * @argc: arrgument count
+ * @argv: argument vector
+ * in a for of 1-d array
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+It all getting more interesting as we deep dive
+into the cores of code and compilition
