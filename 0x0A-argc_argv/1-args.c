f8117463b2107ea2d015fa5f8a3c53d3ab824b09
Manqoba-123 <manoba231@gmail.com>
2023-03-13 06:28:27 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -> prints arguments
+ * @argc: arrgument count
+ * @argv: argument vector
+ * in a for of 1-d array
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
