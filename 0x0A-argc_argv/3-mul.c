abeb6c6c2251a91896e8c38061c95b7995d9fe4d
Manqoba-123 <manoba231@gmail.com>
2023-03-13 07:30:03 -0700

    product

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -> outputs products of numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if  (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
