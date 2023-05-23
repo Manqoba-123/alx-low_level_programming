3d7bbc0595b0b08e632bad41339c9c9e9d2adbae
Manqoba-123 <manoba231@gmail.com>
2023-05-23 13:48:59 -0700

    Adding 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs- Prints absolute value
+ * @int: Integer
+ *
+ * Return: integer
+ */
+
+int _abs(int)
+{
+	int r;
+	int result = (-1) * r;
+
+	return (result);
+}
