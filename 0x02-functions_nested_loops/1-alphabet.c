594aa36e8cbd63e2bba5561144ef1e2a4b86a1b0
Manqoba-123 <manoba231@gmail.com>
2023-05-23 04:52:00 -0700

    adding 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabets
+ * @void: It returns nothing
+ *
+ * Return: aphabets
+ */
+
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+	return (0);
+}
