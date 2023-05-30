48a3e551da47ce235e48b8261d0d5d0b4a25acb8
Manqoba-123 <manoba231@gmail.com>
2023-05-30 13:17:11 -0700

    Adding 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * put2 - prints characters
+ * @str: String
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
