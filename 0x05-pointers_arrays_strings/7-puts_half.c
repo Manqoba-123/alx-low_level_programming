948bc9cd4f563e99caf2cbfd7b6036380f743632
Manqoba-123 <manoba231@gmail.com>
2023-05-30 14:08:54 -0700

    Updating 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,13 @@ void puts_half(char *str)
 	int length = strlen(str);
 	int start = length / 2;
 	int end = length;
-	int i = start;
 
 	if (length % 2 != 0)
 	{
 		start++;
 	}
+	int i = start;
+
 	while (i < end)
 	{
 		_putchar(str[i]);
