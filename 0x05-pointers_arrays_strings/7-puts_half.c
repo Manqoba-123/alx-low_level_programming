f85bd76091d5140783fd1e8b5d093e1bb0eee8b3
Manqoba-123 <manoba231@gmail.com>
2023-05-30 13:57:24 -0700

    Adding 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the last half of the string
+ * @str: String pointer
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start = length / 2;
+	int end = length;
+
+	if (length % 2 != 0)
+	{
+		start++;
+	}
+
+	int i = start;
+
+	while (i < end)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
